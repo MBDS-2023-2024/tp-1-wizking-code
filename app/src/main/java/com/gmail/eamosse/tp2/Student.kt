b155d629ecc9e62feb0bf68091d17515a20f78b2
Jeffking Sterile <jsterile@solutions.ht>
2024-07-23 23:34:06 -0400

    - Créer une classe Etudiant (name, code, sexe (M, F), adresse, age)

--- app/src/main/java/com/gmail/eamosse/tp2/Student.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/Student.kt
@@ -1,6 +1,18 @@
 package com.gmail.eamosse.tp2
 
 //TODO : Complete the class
-data class Student {
-
+data class Student(
+    var name: String,
+    var code: String,
+    var sexe: String,
+    var address: String,
+    var age: Int
+){
+    init {
+        require(sexe == "M" || sexe == "F") { "Sexe must be 'M' or 'F'" }
+        require(age <= 100) { "Age must be under 100" }
+        require(name.length > 1) { "Veuillez entrer un nom" }
+        require(code.length > 1) { "chaque etudiant doit avoir un code" }
+        require(address.length > 1) { "chaque etudiant doit avoir une addresse" }
+    }
 }
